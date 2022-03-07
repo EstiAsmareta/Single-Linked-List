5ed167f4b3ffdde4ad9d338b1860197205e8b1b2
Esti Asmareta Ayu <95353918+EstiAsmareta@users.noreply.github.com>
2022-03-07 22:17:37 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "mahasiswa.h"
+#include "Mahasiswa.h"
 
 using namespace std;
 
@@ -7,11 +7,30 @@ int main()
 {
   single_linked_list L;
 
-  creat_list(&L);
+  create_list(&L);
 
-  address p = alokasi("1234567", "Budiman");
-
+  address p = alokasi("202103", "Oreo Greentea");
   insert_first(&L, p);
+
+  p = alokasi("202104", "Yogurt Chimori");
+  insert_first(&L, p);
+
+  delete_first(&L);
+
+  p = alokasi("202105", "Indomie Kari Ayam");
+  insert_last(&L, p);
+
+  p = alokasi("202105", "Indomi Kari Ayam");
+  insert_last(&L, p);
+
+  delete_last(&L);
+
+  p = alokasi("202106", "Indome Soto");
+  insert_after(&L, p, 1);
+
+  delete_after(&L, 1);
+
+  p = search(L, "202103");
 
   view(L);
 
